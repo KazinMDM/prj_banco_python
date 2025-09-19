f44aad91e1f68013dfae7cc1f5ecc157d125fa8d
Lopez <135629199+KazinMDM@users.noreply.github.com>
2025-09-19 08:27:24 -0300

    atualização

--- prj_banco/main.py
+++ prj_banco/main.py
@@ -1,1 +1,28 @@
-afdfq = 0
+from deposito import deposito
+
+def menu():
+    while True:
+        print("#================ MENU ================#")
+        print("1 - Deposito")
+        print("2 - Saque")
+        print("3 - Extrato")
+        print("0 - Sair")
+
+        opcao = int(input("Digite a opção desejada: "))
+        if opcao != int:
+            print("Opção inválida")
+            menu()
+        else:
+            continue
+
+        if opcao == 1:
+            
+        elif opcao == 2:
+            saque()
+        elif opcao == 3:
+            extrato()
+        elif opcao == 0:
+            break
+
+if __name__ == "__main__":
+    menu()
